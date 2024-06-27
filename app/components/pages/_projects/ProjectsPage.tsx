e51fc782701120c060b9f45db5c15a3be8063207
nandorfn <firnandorfn@gmail.com>
2024-06-27 16:13:06 +0700

    remove add task btn

--- app/components/pages/_projects/ProjectsPage.tsx
+++ app/components/pages/_projects/ProjectsPage.tsx
@@ -121,7 +121,7 @@ const ProjectsPage = ({
             ))
             }
           </div>
-          <Modal
+          {/* <Modal
             title="Create Issue"
             childrenTrigger={
               <Button variant={"primary"}>Add Task</Button>
@@ -143,7 +143,7 @@ const ProjectsPage = ({
                 </Button>
               </DialogClose>
             }
-          />
+          /> */}
 
         </div>
       </div>
